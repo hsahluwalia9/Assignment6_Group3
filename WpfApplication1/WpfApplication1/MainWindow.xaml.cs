31953105c6c78454911c5ae9c68155bf2522bb92
hsahluwalia9 <harsimar.ahluwalia@gmail.com>
2016-10-26 10:23:22 -0700

    Add files via upload

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VM vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new VM();
+            DataContext = vm;
+        }
+
+        private void BtnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            int nofdays = vm.Days;
+            decimal medicalcharge = vm.MedCharge;
+            decimal surgicalcharge = vm.SurgCharge;
+            decimal physicalcharge = vm.PhysCharge;
+            decimal labfees = vm.LabFees;
+
+            decimal total = vm.CalcStayCharges(nofdays) + vm.CalcMiscCharges(medicalcharge, surgicalcharge,labfees, physicalcharge);
+
+            vm.CalcTotalCharge(total);
+
+        }
+    }
+}
---
+++ WpfApplication1/WpfApplication1/VM.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class VM: INotifyPropertyChanged
+    {
+        private int _days=1;
+        public int Days
+        {
+            get { return _days; }
+            set { value = _days; OnPropertyChanged(); }
+        }
+
+        private decimal _medCharge;
+        public decimal MedCharge
+        {
+            get { return _medCharge; }
+            set { value = _medCharge; OnPropertyChanged(); }
+        }
+
+       private decimal _surgCharge;
+        public decimal SurgCharge
+        {
+            get { return _surgCharge; }
+            set { value = _surgCharge; OnPropertyChanged(); }
+        }
+
+       private decimal _labFees;
+        public decimal LabFees
+        {
+            get { return _labFees; }
+            set { value = _labFees; OnPropertyChanged(); }
+        }
+
+       private decimal _physCharge;
+        public decimal PhysCharge
+        {
+            get { return _physCharge; }
+            set { value = _physCharge; OnPropertyChanged(); }
+        }
+
+         public decimal CalcStayCharges(int days)
+        {
+            decimal daycharge;
+            daycharge = days * 350;
+            return daycharge;
+        }
+
+        public decimal CalcMiscCharges(decimal medCharge, decimal surgCharge, decimal labCharge, decimal physCharge)
+        {
+            decimal mischarge;
+            mischarge = medCharge + surgCharge + labCharge + physCharge;
+            return mischarge;
+        }
+
+        public decimal CalcTotalCharge(decimal totalCharge)
+        {
+            decimal total;
+            total = totalCharge;
+            return total;
+        }
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string caller = null)
+        {
+
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+        #endregion
+    }
+}
